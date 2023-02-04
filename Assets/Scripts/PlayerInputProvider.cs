b7c830af0029fbd9e00b439e6dd307c26dbf3f7a
Mika Savolainen <wawe212@hotmail.fi>
2023-02-04 19:57:09 +0200

    score and health

--- Assets/Scripts/PlayerInputProvider.cs
+++ Assets/Scripts/PlayerInputProvider.cs
@@ -3,35 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Linq;
+    public enum AnswerSet {
+        UP = 0,
+        RIGHT = 1,
+        DOWN = 2,
+        LEFT = 3
 
+    }
 public class PlayerInputProvider : MonoBehaviour
 {
+
     public static List<PlayerInputProvider> inputproviders = new List<PlayerInputProvider>();
 
     public int playerIndex;
+    public bool answeredQuestion;
+    public ScoreManager scoreManager;
 
     public void Start(){
-
+        scoreManager = GetComponent<ScoreManager>();
         inputproviders.Add(this);
     }
+    public void Reset(){
+        answeredQuestion = false;
+    }
     public void AnswerRight(InputAction.CallbackContext context){
-        if (context.phase == InputActionPhase.Performed) {
-            Debug.Log("RIGHT :" + playerIndex);
+        if (context.phase == InputActionPhase.Performed && !answeredQuestion) {
+            var retval = QuestionManager.AnswerQuestion(AnswerSet.RIGHT);
+            if(retval){
+                scoreManager.score++;
+            }
+            else {
+                scoreManager.health++;
+            }
         }
     }
     public void AnswerDown(InputAction.CallbackContext context){
-        if (context.phase == InputActionPhase.Performed) {
-            Debug.Log("DOWN :"+ playerIndex);
+        if (context.phase == InputActionPhase.Performed && !answeredQuestion) {
+            var retval = QuestionManager.AnswerQuestion(AnswerSet.DOWN);
+            if(retval){
+                scoreManager.score++;
+            }
+            else {
+                scoreManager.health++;
+            }
         }
     }
     public void AnswerLeft(InputAction.CallbackContext context){
-        if (context.phase == InputActionPhase.Performed) {
-            Debug.Log("LEFT :"+ playerIndex);
+        if (context.phase == InputActionPhase.Performed && !answeredQuestion) {
+            var retval = QuestionManager.AnswerQuestion(AnswerSet.LEFT);
+            if(retval){
+                scoreManager.score++;
+            }            
+            else {
+                scoreManager.health++;
+            }
         }
     }
     public void AnswerUp(InputAction.CallbackContext context){
-        if (context.phase == InputActionPhase.Performed) {
-            Debug.Log("UP :"+ playerIndex);
+        if (context.phase == InputActionPhase.Performed && !answeredQuestion) {
+           var retval =  QuestionManager.AnswerQuestion(AnswerSet.UP);
+            if(retval){
+                scoreManager.score++;
+            }
+            else {
+                scoreManager.health++;
+            }
         }
     }
 }
