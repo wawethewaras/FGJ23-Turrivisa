b7eaa92ead506ca7bc9d00c7f9a0f4acbd501588
Mika Savolainen <wawe212@hotmail.fi>
2023-02-04 13:25:58 +0200

    Inital commit

---
+++ Assets/Scripts/CharacterSelector.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelector : MonoBehaviour
+{
+    public List<int> selectFrom = new List<int>();
+
+    public List<Sprite> characters = new List<Sprite>();
+    private int currentIndex = 0;
+
+    public Image image;
+
+    public void ChangeCharacterForward() {
+        currentIndex++;
+        if(currentIndex >= characters.Count){
+            currentIndex = 0;
+        }
+        image.sprite = characters[currentIndex];
+    }
+        public void ChangeCharacterBackward() {
+        currentIndex--;
+        if(currentIndex < 0){
+            currentIndex = characters.Count- 1;
+        }
+        image.sprite = characters[currentIndex];
+    }
+}
