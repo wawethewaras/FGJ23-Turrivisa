cd40ae43d06523cfedef1467ca43566fd0f2fee6
Mika Savolainen <wawe212@hotmail.fi>
2023-02-05 13:24:29 +0200

    winscreen

--- Assets/Scripts/Managers/Scenes.cs
+++ Assets/Scripts/Managers/Scenes.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Scenes : MonoBehaviour
 {
-    public void LoadNewScene(string sceneName)
+    public static void LoadNewScene(string sceneName)
     {
         PlayerInputProvider.inputproviders.Clear();
         SceneManager.LoadScene(sceneName);
